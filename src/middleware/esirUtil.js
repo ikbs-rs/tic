c57102a2f2e05883b574cde4738f045951357b5f
Filip <filip.z.radulovic@gmail.com>
2024-03-14 21:24:45 +0100

    Add esir assembler for invoices

---
+++ src/middleware/esirUtil.js
@@ -0,0 +1,69 @@
+const assembleEsirInvoicesRequestBody = (body) => {
+    return {
+        advancePaid: null,  //?? Koristi se za izdavanje zavrsnog avansnog racuna, opcioni parametar, sadrzi ukupno placeni avans
+        advanceTax: null,  //?? Koristi se za izdavanje zavrsnog avansnog racuna, opcioni parametar, sadrzi vrednost poreza na avans
+        invoiceRequest: {
+            buyerCostCenterId: body?.buyerCostCenterId, //??
+            buyerId: body?.userId,//??
+            cashier: body?.cashier,//??
+            dateAndTimeOfIssue: body?.date,  //tic_doc.date
+            invoiceNumber: body?.broj,     //tic_doc.broj
+            invoiceType: "Normal", //??
+            referentDocumentDT: null,     //?
+            referentDocumentNumber: null,     //?
+            transactionType: "Sale", //??
+            items: body.tic_docs.map(tic_doc => {
+                return {
+                    gtin: tic_doc.id,       // (tic_docs.id)
+                    labels: [
+                        "A"
+                    ],
+                    name: tic_doc.art,// (tic_docs.art) -> tic_art.text
+                    quantity: tic_doc.output,      // tic_docs.output
+                    totalAmount: tic_doc.totalAmount, //??
+                    unitPrice: tic_doc.price   // tic_docs.price
+                }
+            }),
+            payment: [
+                {
+                    "amount": body?.amount,   // Ovo mora da se izracuna
+                    "paymentType": "Card"
+                }
+            ]
+        }
+    };
+};
+
+
+const assembleEsirInvoicesResponse = (esirInvoicesResponse) => {
+    return {
+        "requestedBy": esirInvoicesResponse?.requestedBy,
+        "signedBy": esirInvoicesResponse?.signedBy,
+        "sdcDateTime": esirInvoicesResponse?.sdcDateTime,
+        "invoiceCounter": esirInvoicesResponse?.invoiceCounter,
+        "invoiceCounterExtension": esirInvoicesResponse?.invoiceCounterExtension,
+        "invoiceNumber": esirInvoicesResponse?.invoiceNumber,
+        "verificationUrl": esirInvoicesResponse?.verificationUrl,
+        "verificationQRCode": esirInvoicesResponse?.verificationQRCode,
+        "journal": esirInvoicesResponse?.journal,
+        "totalCounter": esirInvoicesResponse?.totalCounter,
+        "transactionTypeCounter": esirInvoicesResponse?.transactionTypeCounter,
+        "totalAmount": esirInvoicesResponse?.totalAmount,
+        "encryptedInternalData": esirInvoicesResponse?.encryptedInternalData,
+        "signature": esirInvoicesResponse?.signature,
+        "taxItems": esirInvoicesResponse?.taxItems,
+        "businessName": esirInvoicesResponse?.businessName,
+        "locationName": esirInvoicesResponse?.locationName,
+        "address": esirInvoicesResponse?.address,
+        "tin": esirInvoicesResponse?.tin,
+        "district": esirInvoicesResponse?.district,
+        "taxGroupRevision": esirInvoicesResponse?.taxGroupRevision,
+        "mrc": esirInvoicesResponse?.mrc,
+        "messages": esirInvoicesResponse?.messages,
+        "invoiceImagePngBase64": esirInvoicesResponse?.invoiceImagePngBase64,
+        "invoiceImagePdfBase64": esirInvoicesResponse?.invoiceImagePdfBase64,
+        "invoiceImageHtml": esirInvoicesResponse?.invoiceImageHtml
+    };
+};
+
+export default {assembleEsirInvoicesRequestBody, assembleEsirInvoicesResponse};
