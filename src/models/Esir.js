0b0c1fcb29e8b026831aa2833a2ac5d5ad617bea
Filip <filip.z.radulovic@gmail.com>
2024-05-26 14:13:48 +0200

    checkSettings and setEsirSettings

--- src/models/Esir.js
+++ src/models/Esir.js
@@ -1,10 +1,22 @@
 import axios from "axios";
 
+const getEsirSettings = async (url, config) => {
+  const rows = await axios.get(url, config);
+  return rows
+};
+
+const setEsirSettings = async (url, body, config) => {
+  const rows = await axios.post(url, body, config);
+  return rows
+};
+
 const postEsirInvoices = async (url, body, config) => {
   const rows = await axios.post(url, body, config);
   return rows
 };
 
 export default {
+  getEsirSettings,
+  setEsirSettings,
   postEsirInvoices,
 };
